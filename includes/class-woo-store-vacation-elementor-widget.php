a319454ff1f6fa5591af951e070d5c2abeeff0eb
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-13 17:49:16 +0300

    feat: Avoid reloading preview

--- includes/class-woo-store-vacation-elementor-widget.php
+++ includes/class-woo-store-vacation-elementor-widget.php
@@ -50,6 +50,20 @@ if (
 		}
 
 		/**
+		 * Whether the reload preview is required or not.
+		 *
+		 * Used to determine whether the reload preview is required.
+		 *
+		 * @since 1.8.1
+		 *
+		 * @return bool Whether the reload preview is required.
+		 */
+		public function is_reload_preview_required() {
+
+			return false;
+		}
+
+		/**
 		 * Get widget icon.
 		 *
 		 * @since 1.8.1
@@ -86,6 +100,57 @@ if (
 		}
 
 		/**
+		 * Register divider widget controls.
+		 *
+		 * Adds different input fields to allow the user to change and customize the widget settings.
+		 *
+		 * @since 1.8.1
+		 *
+		 * @return void
+		 */
+		protected function register_controls() {
+
+			$this->start_controls_section(
+				'section_' . Woo_Store_Vacation::SHORTCODE,
+				array(
+					'label' => $this->get_title(),
+				)
+			);
+
+			$this->end_controls_section();
+		}
+
+		/**
+		 * Remove the widget stack of controls.
+		 *
+		 * @since 1.8.1
+		 *
+		 * @param bool $with_common_controls Optional. Whether to include the common controls. Default is true.
+		 *
+		 * @return array Empty widget stack of controls.
+		 */
+		public function get_stack( $with_common_controls = true ) {
+
+			return parent::get_stack( false );
+		}
+
+		/**
+		 * Render shortcode widget as plain content.
+		 *
+		 * Override the default behavior by printing the shortcode instead of rendering it.
+		 *
+		 * @since 1.8.1
+		 *
+		 * @return void
+		 */
+		public function render_plain_content() {
+
+			// In plain mode, render the shortcode tag.
+			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo '[' . Woo_Store_Vacation::SHORTCODE . ']';
+		}
+
+		/**
 		 * Render the widget output in the editor.
 		 *
 		 * @since 1.8.1
@@ -100,7 +165,18 @@ if (
 				return;
 			}
 
-			echo do_shortcode( '[' . Woo_Store_Vacation::SHORTCODE . ']' );
+			echo do_shortcode( shortcode_unautop( '[' . Woo_Store_Vacation::SHORTCODE . ']' ) );
 		}
+
+		/**
+		 * Render shortcode widget output in the editor.
+		 *
+		 * Written as a Backbone JavaScript template and used to generate the live preview.
+		 *
+		 * @since 1.8.1
+		 *
+		 * @return void
+		 */
+		protected function content_template() {}
 	}
 endif;
