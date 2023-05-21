eeb0eb060b53c426a3dbc9dd7cbe60922a64ac86
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-21 23:59:40 +0300

    refactor: Ask for review in the footer

--- src/Settings/Register.php
+++ src/Settings/Register.php
@@ -11,6 +11,9 @@
 
 namespace Woo_Store_Vacation\Settings;
 
+use WP_Footer_Rate\Rate;
+use Woo_Store_Vacation\Helper;
+
 /**
  * Register plugin settings fields.
  */
@@ -26,6 +29,7 @@ class Register {
 	public function setup() {
 
 		add_filter( 'woocommerce_get_settings_pages', array( $this, 'settings' ) );
+		add_action( 'woocommerce_settings_start', array( $this, 'rate' ) );
 	}
 
 	/**
@@ -42,5 +46,22 @@ class Register {
 
 		$settings[] = woo_store_vacation()->service( 'settings' );
 		return $settings;
+	}
+
+	/**
+	 * Ask for a review in the footer of the settings page.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function rate() {
+
+		new Rate(
+			woo_store_vacation()->service( 'file' )->plugin_basename(),
+			woo_store_vacation()->get_slug(),
+			_x( 'Woo Store Vacation', 'plugin name', 'woo-store-vacation' ),
+			Helper\Settings::is_page()
+		);
 	}
 }
