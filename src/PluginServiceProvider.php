81675e593c8c3c57223e86d9d241960d110fedce
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-21 15:01:14 +0300

    feat: Pimple service provider interface

---
+++ src/PluginServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The implementation of the Pimple service provider interface.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.0.0
+ *
+ * @package woo-store-vacation
+ */
+
+namespace Woo_Store_Vacation;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Woo_Store_Vacation\Settings;
+use Woo_Store_Vacation\Shortcode;
+
+/**
+ * Class PluginServiceProvider.
+ */
+class PluginServiceProvider implements ServiceProviderInterface {
+
+	/**
+	 * Registers services on the given container.
+	 *
+	 * This method should only be used to configure services and parameters.
+	 * It should not get services.
+	 *
+	 * @param Container $pimple Container instance.
+	 */
+	public function register( Container $pimple ): void {
+
+		// Plugin settings.
+		$pimple['settings'] = fn() => new Settings\Settings();
+		$pimple['options']  = fn() => new Settings\Options();
+
+		// Shortcodes.
+		$pimple['notice'] = fn() => new Shortcode\Notice();
+	}
+}
