1ea1a7996b539c2909d44ee098c08ec417bf5df5
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-22 17:38:28 +0300

    refactor: Dismiss up-sell admin notice

---
+++ src/Ajax/Upsell.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Dismiss up-sell admin notice.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.3.8
+ *
+ * @package woo-store-vacation
+ */
+
+namespace Woo_Store_Vacation\Ajax;
+
+use Woo_Store_Vacation\Enhancements;
+
+/**
+ * Upsell admin notice class.
+ */
+class Upsell extends Ajax {
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.3.8
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+		// Parent constructor.
+		parent::__construct(
+			'woo_store_vacation_dismiss_upsell',
+			Enhancements\Notices::DISMISS_NONCE_NAME
+		);
+	}
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.3.8
+	 *
+	 * @return void
+	 */
+	public function setup() {
+
+		$this->register_admin();
+	}
+
+	/**
+	 * AJAX dismiss up-sell admin notice.
+	 *
+	 * @since 1.3.8
+	 *
+	 * @return void
+	 */
+	public function ajax_callback() {
+
+		// Bail early if the nonce is invalid.
+		$this->verify_nonce();
+
+		// Set the upsell transient.
+		set_transient( Enhancements\Upsell::TRANSIENT_NAME, true, MONTH_IN_SECONDS );
+
+		exit();
+	}
+}
