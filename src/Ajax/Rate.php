a78b3a83eea2a1f2a6d64cf8e1cfa492dea4f410
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-23 15:03:19 +0300

    refactor: Dismiss rate the plugin admin notice

---
+++ src/Ajax/Rate.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Dismiss rate the plugin admin notice.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.6.1
+ *
+ * @package woo-store-vacation
+ */
+
+namespace Woo_Store_Vacation\Ajax;
+
+use Woo_Store_Vacation\Enhancements;
+
+/**
+ * Rate admin notice class.
+ */
+class Rate extends Ajax {
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.6.1
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+		// Parent constructor.
+		parent::__construct(
+			'woo_store_vacation_dismiss_rate',
+			Enhancements\Notices::DISMISS_NONCE_NAME
+		);
+	}
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.6.1
+	 *
+	 * @return void
+	 */
+	public function setup() {
+
+		$this->register_admin();
+	}
+
+	/**
+	 * AJAX dismiss rate admin notice.
+	 *
+	 * @since 1.6.1
+	 *
+	 * @return void
+	 */
+	public function ajax_callback() {
+
+		// Bail early if the nonce is invalid.
+		$this->verify_nonce();
+
+		// Set the upsell transient.
+		set_transient( Enhancements\Rate::TRANSIENT_NAME, true, 3 * MONTH_IN_SECONDS );
+
+		exit();
+	}
+}
