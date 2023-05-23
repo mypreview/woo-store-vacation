5ac5849a707b1e7f148d1345e9db4d48d7399b54
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-23 15:06:43 +0300

    refactor: Move menu register method to its own class

--- src/Settings/Register.php
+++ src/Settings/Register.php
@@ -11,9 +11,6 @@
 
 namespace Woo_Store_Vacation\Settings;
 
-use WP_Footer_Rate\Rate;
-use Woo_Store_Vacation\Helper;
-
 /**
  * Register plugin settings fields.
  */
@@ -28,30 +25,7 @@ class Register {
 	 */
 	public function setup() {
 
-		add_action( 'admin_menu', array( $this, 'add_submenu_page' ), 999 );
 		add_filter( 'woocommerce_get_settings_pages', array( $this, 'settings' ) );
-		add_action( 'woocommerce_settings_start', array( $this, 'rate' ) );
-	}
-
-	/**
-	 * Create plugin options page.
-	 * Backward compatibility for old location of the plugin’s settings page.
-	 *
-	 * @since 1.0.0
-	 * @deprecated 1.8.0
-	 *
-	 * @return void
-	 */
-	public function add_submenu_page() {
-
-		add_submenu_page(
-			'woocommerce',
-			esc_html_x( 'Woo Store Vacation', 'plugin name', 'woo-store-vacation' ),
-			esc_html_x( 'Store Vacation', 'menu title', 'woo-store-vacation' ),
-			'manage_woocommerce',
-			woo_store_vacation()->get_slug(),
-			fn() => wp_safe_redirect( Helper\Settings::page_uri() )
-		);
 	}
 
 	/**
@@ -68,22 +42,5 @@ class Register {
 
 		$settings[] = woo_store_vacation()->service( 'settings' );
 		return $settings;
-	}
-
-	/**
-	 * Ask for a review in the footer of the settings page.
-	 *
-	 * @since 1.8.0
-	 *
-	 * @return void
-	 */
-	public function rate() {
-
-		new Rate(
-			woo_store_vacation()->service( 'file' )->plugin_basename(),
-			woo_store_vacation()->get_slug(),
-			_x( 'Woo Store Vacation', 'plugin name', 'woo-store-vacation' ),
-			Helper\Settings::is_page()
-		);
 	}
 }
