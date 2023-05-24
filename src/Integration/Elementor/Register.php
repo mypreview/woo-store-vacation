858c6e8fbbeafd9d84c84537a63a2d96a2f77f8e
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-24 11:50:39 +0300

    refactor: Register elementor widget

---
+++ src/Integration/Elementor/Register.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Elementor widget registerer.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.8.1
+ *
+ * @package woo-store-vacation
+ */
+
+namespace Woo_Store_Vacation\Integration\Elementor;
+
+/**
+ * Class Register.
+ */
+class Register {
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.8.1
+	 *
+	 * @return void
+	 */
+	public function setup() {
+
+		add_action( 'elementor/widgets/widgets_registered', array( $this, 'widget' ) );
+	}
+
+	/**
+	 * Register the widget.
+	 *
+	 * @since 1.8.1
+	 *
+	 * @param object $widget_manager The widget manager object.
+	 *
+	 * @return void
+	 */
+	public function widget( $widget_manager ) {
+
+		// Register the widget.
+		$widget_manager->register_widget_type( woo_store_vacation()->service( 'elementor' ) );
+	}
+}
