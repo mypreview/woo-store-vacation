1aba4c9cc7dbafcc00bcbf22e9ccc1e66cf57e0b
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-26 22:11:17 +0300

    feat: The Template for displaying onboarding notice

---
+++ templates/onboarding-notice.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The Template for displaying onboarding (welcome) admin notice.
+ *
+ * @since 1.0.0
+ *
+ * @package woo-store-vacation
+ */
+
+defined( 'ABSPATH' ) || exit;
+defined( 'WC_VERSION' ) || exit;
+
+?>
+
+<div id="<?php echo esc_attr( woo_store_vacation()->get_slug() ); ?>-dismiss-onboarding" class="notice notice-info is-dismissible" data-action="onboarding">
+	<p>
+		<i class="dashicons dashicons-admin-settings"></i>
+		<?php
+		printf(
+			/* translators: 1: Plugin name, 2: Open anchor tag, 3: Close anchor tag. */
+			esc_html_x( 'Thanks for installing %2$s plugin! To get started, visit the %3$splugin’s settings page%4$s.', 'admin notice', 'woo-store-vacation' ),
+			'<i class="dashicons dashicons-admin-settings"></i>',
+			sprintf(
+				'<strong>%s</strong>',
+				esc_html_x( 'Woo Store Vacation', 'plugin name', 'woo-store-vacation' )
+			),
+			sprintf(
+				'<a href="%s" class="notice-dismiss-later" target="_self">',
+				esc_url( $args['uri'] )
+			),
+			'</a>'
+		);
+		?>
+	</p>
+</div>
