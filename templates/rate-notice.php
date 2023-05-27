e7cc4cc34262c22f8f7a2931f7c9243010b78125
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-27 14:40:33 +0300

    perf: Remove calculation of usage timestamp
    fix: Remove call for the get_slug function

--- templates/rate-notice.php
+++ templates/rate-notice.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 defined( 'WC_VERSION' ) || exit;
 
 ?>
-<div id="<?php echo esc_attr( woo_store_vacation()->get_slug() ); ?>-dismiss-rate" class="notice notice-alt is-dismissible" data-action="rate">
+<div id="woo-store-vacation-dismiss-rate" class="notice notice-alt is-dismissible" data-action="rate">
 	<p>
 		<i class="dashicons dashicons-star-filled"></i>
 		<strong>
@@ -19,7 +19,7 @@ defined( 'WC_VERSION' ) || exit;
 			printf(
 				/* translators: 1: Activation duration, 2: Plugin name */
 				esc_html_x( '%1$s have passed since you started using %2$s.', 'admin notice', 'woo-store-vacation' ),
-				esc_html( human_time_diff( woo_store_vacation()->service( 'options' )->get_usage_timestamp() ) ),
+				esc_html( $args['usage_timestamp'] ),
 				esc_html_x( 'Woo Store Vacation', 'plugin name', 'woo-store-vacation' )
 			);
 			?>
@@ -29,7 +29,7 @@ defined( 'WC_VERSION' ) || exit;
 		<?php echo esc_html_x( ' Would you kindly consider leaving a review and letting us know how the plugin has helped your business? Your feedback is greatly appreciated!', 'admin notice', 'woo-store-vacation' ); ?>
 	</p>
 	<p>
-		<a href="https://wordpress.org/support/plugin/<?php echo esc_attr( woo_store_vacation()->get_slug() ); ?>/reviews?filter=5#new-post" class="button-primary notice-dismiss-later" target="_blank" rel="noopener noreferrer nofollow">
+		<a href="https://wordpress.org/support/plugin/woo-store-vacation/reviews?filter=5#new-post" class="button-primary notice-dismiss-later" target="_blank" rel="noopener noreferrer nofollow">
 			&#9733;
 			<?php echo esc_html_x( 'Give 5 Stars', 'admin notice', 'woo-store-vacation' ); ?> &#8594;
 		</a>
