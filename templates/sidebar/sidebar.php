4446e0d21f49995fc9047b5489347451bb1c3ade
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-06-07 15:59:27 +0300

    feat: Template for displaying settings page sidebar

---
+++ templates/sidebar/sidebar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The Template for displaying settings page sidebar.
+ *
+ * @since 1.0.0
+ *
+ * @package woo-store-vacation
+ */
+
+defined( 'ABSPATH' ) || exit;
+defined( 'WC_VERSION' ) || exit;
+
+?>
+<div class="woo-store-vacation-page-sidebar">
+	<?php
+	/**
+	 * Fires inside the sidebar.
+	 *
+	 * @since 1.0.0
+	 */
+	do_action( 'woo_store_vacation_settings_sidebar' );
+	?>
+</div>
