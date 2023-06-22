beba177396c4e1328c59dd6df1539397ea4770ff
Sajedeh Gooklani <gookalani@mail.com>
2023-06-22 20:11:17 +0300

    Update WordPress and WooCommerce required versions

--- woo-store-vacation.php
+++ woo-store-vacation.php
@@ -29,13 +29,13 @@
  * Version:              1.9.1
  * Author:               MyPreview
  * Author URI:           https://mypreview.one/woo-store-vacation
- * Requires at least:    5.3
+ * Requires at least:    5.5
  * Requires PHP:         7.4
  * License:              GPL-3.0
  * License URI:          http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:          woo-store-vacation
  * Domain Path:          /languages
- * WC requires at least: 4.0
+ * WC requires at least: 5.5
  * WC tested up to:      7.8
  */
 
